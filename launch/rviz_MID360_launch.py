96e10354d4ff06d43a830de64d942594c3149a91
effibot <andrea.efficace1@gmail.com>
2025-01-20 17:26:48 +0100

    [F] update to driver

--- launch/rviz_MID360_launch.py
+++ launch/rviz_MID360_launch.py
@@ -7,7 +7,7 @@ from launch.substitutions import LaunchConfiguration
 import launch
 
 ################### user configure parameters for ros2 start ###################
-xfer_format   = 5    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format, 5-MID360 PC2 MSG
+xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format, 5-MID360 PC2 MSG
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
 publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
@@ -23,7 +23,7 @@ user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
 ################### user configure parameters for ros2 end #####################
 
 livox_ros2_params = [
-    # {"xfer_format": xfer_format},
+    {"xfer_format": xfer_format},
     {"multi_topic": multi_topic},
     {"data_src": data_src},
     {"publish_freq": publish_freq},
